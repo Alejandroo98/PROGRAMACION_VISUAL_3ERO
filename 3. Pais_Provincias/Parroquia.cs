80eb5e99a957a821665fcd335f10fb105df4a077
Alejandro <jeffersonaleandro27@gmail.com>
2021-06-23 14:19:39 -0500

    Clase 22-06-2021 con comentarios

---
+++ 3. Pais_Provincias/Parroquia.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+namespace Clase_22_06_2021
+{
+    class Parroquia
+    {
+        public int ParroquiaId
+        {
+            get; set;
+        }
+        public string Nombre
+        {
+            get; set;
+        }
+        public int Habitantes
+        {
+            get; set;
+        }
+    }
+
+
+}
